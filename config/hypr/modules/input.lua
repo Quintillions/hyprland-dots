c5398db8fcebef21a6e11f8cd626dcc04e2f2e6a
Quin <lamanocordero@gmail.com>
2026-06-17 17:23:23 +0800

    us keybaord

--- config/hypr/modules/input.lua
+++ config/hypr/modules/input.lua
@@ -1,6 +1,6 @@
 hl.config({
     input = {
-        kb_layout          = "de",
+        kb_layout          = "us",
         follow_mouse       = 1,
         sensitivity        = 0,
         accel_profile      = "flat",
