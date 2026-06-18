0f849f86e9a08676092a3735fcfd5183d3e152cf
Quin <lamanocordero@gmail.com>
2026-06-18 18:26:36 +0800

    revert

--- config/hypr/modules/binds.lua
+++ config/hypr/modules/binds.lua
@@ -7,7 +7,6 @@ hl.bind(mod .. " + F",         hl.dsp.window.fullscreen())
 hl.bind(mod .. " + E",         hl.dsp.exec_cmd("dolphin"))
 hl.bind(mod .. " + T",         hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mod .. " + M",         hl.dsp.window.move({ workspace = "special:minimized", follow = false }))
-hl.bind(mod .. " + SHIFT + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 
 hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
@@ -19,15 +18,15 @@ hl.bind(mod .. " + mouse_down", hl.dsp.focus({ workspace = "r+1" }))
 
 hl.bind(mod .. " + SHIFT + C",  hl.dsp.exec_cmd("hyprpicker -a"))
 
-hl.bind(mod .. " + Space",      hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/launcher.sh"))
-hl.bind(mod .. " + V",          hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/clipboard.sh"))
+hl.bind(mod .. " + Space",      hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/launcher.sh"))
+hl.bind(mod .. " + V",          hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/clipboard.sh"))
 
-hl.bind(mod .. " + SUPER_L",    hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/link.sh"), { release = true })
+hl.bind(mod .. " + SUPER_L",    hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/link.sh"), { release = true })
 
-hl.bind(mod .. " + L",          hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/lock.sh"))
+hl.bind(mod .. " + L",          hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/lock.sh"))
 
-hl.bind(mod .. " + B",          hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/wallpaper.sh"))
-hl.bind(mod .. " + C",          hl.dsp.exec_cmd(os.getenv("HOME") .. "./config/hypr/scripts/wallpaper-picker.sh"))
+hl.bind(mod .. " + B",          hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/wallpaper.sh"))
+hl.bind(mod .. " + C",          hl.dsp.exec_cmd(os.getenv("HOME") .. "/.config/hypr/scripts/wallpaper-picker.sh"))
 
 hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
 hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
