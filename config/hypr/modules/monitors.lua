ca50e3dc71bbe388cdeb176b67f326a4e621d27e
Quin <lamanocordero@gmail.com>
2026-06-18 11:03:19 +0800

    fix monitors

--- config/hypr/modules/monitors.lua
+++ config/hypr/modules/monitors.lua
@@ -1,13 +1,13 @@
 hl.monitor({
     output   = "DP-1",
-    mode     = "2560x1440@280",
+    mode     = "1980x1080@180",
     position = "2560x0",
     scale    = 1,
 })
 
 hl.monitor({
     output   = "HDMI-A-1",
-    mode     = "2560x1440@144",
+    mode     = "1980x1080@180",
     position = "0x0",
     scale    = 1,
 })
