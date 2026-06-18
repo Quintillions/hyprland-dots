a1cfdd492f50e77b992353df8728dd9d68e0fae3
Quin <lamanocordero@gmail.com>
2026-06-18 11:45:59 +0800

    outfoxxed

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
 
     quickshell = {
-      url = "github:outfoxxed/quickshell";
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     
