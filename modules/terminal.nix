12f524c237ddfef897434520f07c5a2d68438178
Quin <lamanocordero@gmail.com>
2026-06-19 20:30:34 +0800

    hypridle does not work

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -49,41 +49,6 @@
     };
   };
 
-  services.hypridle = {
-    enable = true;
-
-    settings = {
-      general = {
-        lock_cmd = "qs -c lock ipc call lock lock";
-        before_sleep_cmd = "qs -c lock ipc call lock lock";
-        after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
-      };
-
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "brightnessctl set 10%";
-          on-resume = "brightnessctl set 100%";
-        }
-
-        {
-          timeout = 450;
-          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
-          on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
-        }
-
-        {
-          timeout = 600;
-          on-timeout = "qs -c lock ipc call lock lock";
-        }
-
-        {
-          timeout = 900;
-          on-timeout = "systemctl suspend";
-        }
-      ];
-    };
-  };
 
   programs.alacritty = {
     enable = true;
