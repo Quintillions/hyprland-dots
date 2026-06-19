b1a36abe2cb1f568fd9a29ac7bef9439240e06ec
Quin <lamanocordero@gmail.com>
2026-06-19 19:28:37 +0800

    fixed monitors

--- config/hypr/modules/monitors.lua
+++ config/hypr/modules/monitors.lua
@@ -1,21 +1,12 @@
 hl.monitor({
-    output   = "DP-1",
-    mode     = "1980x1080@180",
-    position = "2560x0",
-    scale    = 1,
-})
-
-hl.monitor({
-    output   = "HDMI-A-1",
-    mode     = "1980x1080@180",
+    output   = "eDP-1",
+    mode     = "2560x1440@180",
     position = "0x0",
     scale    = 1,
 })
 
+
 for i = 1, 5 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-1" })
+    hl.workspace_rule({ workspace = tostring(i), monitor = "eDP-1" })
 end
 
-for i = 6, 10 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "HDMI-A-1" })
-end
