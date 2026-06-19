dcfd66f2b029944228862e6b6397a9e7cf7cf59d
Quin <lamanocordero@gmail.com>
2026-06-19 19:26:37 +0800

    commented out nvidia

--- config/hypr/modules/env.lua
+++ config/hypr/modules/env.lua
@@ -2,10 +2,10 @@ hl.env("XCURSOR_THEME",   "Bibata-Modern-Ice")
 hl.env("XCURSOR_SIZE",    "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
-hl.env("LIBVA_DRIVER_NAME",         "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("__GL_GSYNC_ALLOWED",        "0")
-hl.env("__GL_VRR_ALLOWED",          "0")
+-- hl.env("LIBVA_DRIVER_NAME",         "iHD")
+-- hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+-- hl.env("__GL_GSYNC_ALLOWED",        "0")
+--hl.env("__GL_VRR_ALLOWED",          "0")
 
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
